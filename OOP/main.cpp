69ca968eb7e3b86a82f466b2c66df94a813c1b92
Oluwatobi Emmanuel <perspectiveguy35@gmail.com>
2025-01-17 03:21:19 -0600

    managing objects through pointers

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -8,5 +8,10 @@ int main()
     Cylinder cylinder1(10, 10);
     cout << "Volume: " << cylinder1.volume() << endl;
 
+    // Managing a stack object through pointers
+    Cylinder* p_cylinder1 = &cylinder1;
+
+    cout << "Volume: " << p_cylinder1->volume() << endl;
+
     return 0;
 }
